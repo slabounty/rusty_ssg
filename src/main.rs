d727f89f1096016f57f7518312401bb703de7328
Scott LaBounty <slabounty@Scotts-MacBook-Air.local>
2025-09-12 12:56:33 -0700

    Add Code for md Files
    * Add code to search content directly for md files
    * Print filenames out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::ffi::OsStr;
+use walkdir::WalkDir;
+
 fn main() {
-    println!("Hello, world!");
+    println!("List Markdown files in content directory");
+    for entry in WalkDir::new("./content")
+        .into_iter()
+        .filter_map(|e| e.ok()) // Ignore any errors during traversal
+        .filter(|e| {
+            // First, check if the entry is a file
+            e.file_type().is_file() &&
+            // Then, check if the file has the ".md" extension
+            e.path().extension().and_then(OsStr::to_str) == Some("md")
+        })
+    {
+        println!("{}", entry.path().display());
+    }
 }
