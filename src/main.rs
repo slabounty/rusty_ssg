8a95700b50c81dfacfa7401d1603e40f53a44a86
Scott LaBounty <slabounty@Scotts-MacBook-Air.local>
2025-09-14 10:40:39 -0700

    Use Template for HTML Generation
    * Reorganize toml file
    * Set up templates/base.html
    * Use tera to generate hmtl

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs;
 use std::ffi::OsStr;
-use walkdir::WalkDir;
 use pulldown_cmark::{Parser, Options, html};
+use tera::{Tera, Context};
+use walkdir::WalkDir;
 
 
 fn main() {
@@ -9,14 +10,14 @@ fn main() {
 
     // At somepoint make it optional to get from the command line.
     let content = String::from("./content");
-    let template = String::from("./template");
+    let template = String::from("./templates/*.html");
     let output = String::from("./output");
 
     // Convert the files in content with the template files and put them in the output directory.
     convert_files(&content, &template, &output);
 }
 
-fn convert_files(content_dir: &String, _template_dir: &str, _output_dir: &str) {
+fn convert_files(content_dir: &str, template_dir: &str, _output_dir: &str) {
     println!("List Markdown files in {} directory", content_dir);
     for entry in WalkDir::new(content_dir)
         .into_iter()
@@ -28,20 +29,20 @@ fn convert_files(content_dir: &String, _template_dir: &str, _output_dir: &str) {
             e.path().extension().and_then(OsStr::to_str) == Some("md")
         })
     {
-        convert_file_to_html(&entry.path().display().to_string());
+        convert_file_to_html(&entry.path().display().to_string(), template_dir);
     }
 }
 
-fn convert_file_to_html(md_file_path: &String) {
+fn convert_file_to_html(md_file_path: &str, template_dir: &str) {
     println!("Markdown file = {}", md_file_path);
     let markdown_input = fs::read_to_string(md_file_path);
     match markdown_input {
-        Ok(markdown_text) => convert_md_text_to_html(&markdown_text),
+        Ok(markdown_text) => convert_md_text_to_html(&markdown_text, template_dir),
         Err(e) => println!("Operation failed: {}", e), // std::io::Error implements Display
     }
 }
 
-fn convert_md_text_to_html(markdown_text: &String) {
+fn convert_md_text_to_html(markdown_text: &str, template_dir: &str) {
     println!("Markdown text = \n{}", markdown_text);
 
     // Set up options (e.g., enable tables, footnotes, etc.)
@@ -58,4 +59,14 @@ fn convert_md_text_to_html(markdown_text: &String) {
 
     // Print or save the HTML output
     println!("HTML output =\n{}", html_output);
+
+    let base_html = String::from("base.html");
+    let tera = Tera::new(template_dir).unwrap();
+
+    let mut context = Context::new();
+    context.insert("title", "The Title"); // Insert the generated HTML content
+    context.insert("content", &html_output); // Insert the generated HTML content
+
+    let rendered_html = tera.render(&base_html, &context).unwrap();
+    println!("Rendered html = \n {}", rendered_html);
 }
